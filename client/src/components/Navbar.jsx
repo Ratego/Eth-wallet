157e278b4462fba87d4d0714565985a01679a283
Ratego <advanceddeveloper2020@gmail.com>
2022-04-11 12:22:05 +0300

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+
+const Navbar = () => {
+    return (
+        <h1>Navbar</h1>
+    );
+}
+export default Navbar;
