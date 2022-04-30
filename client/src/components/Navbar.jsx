7d0b6b353c381036aec96566728f7acf661ea32d
Ratego <advanceddeveloper2020@gmail.com>
2022-04-30 15:13:11 +0300

    second update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { HiMenuAlt4 } from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
@@ -11,7 +11,7 @@ const NavbarItem = ({title, classProps }) => {
     )
 }
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
@@ -25,7 +25,7 @@ const Navbar = () => {
                     Login
                 </li>
             </ul>
-            <div className='flex relative'>
+            <div className="flex relative">
                 {!toggleMenu && (
                     <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
                 )}
@@ -34,13 +34,13 @@ const Navbar = () => {
                 )}
                 {toggleMenu && (
                     <ul
-                        className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                        className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
                         flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
                         <li className="text-xl w-full my-2">
                             <AiOutlineClose onClick={() => setToggleMenu(false)} />
                         </li>
                         {["Market", "Exchange", "Tutorials", "Wallets"].map(
-                        (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+                        (item, index) => <NavbarItem key={item + index} title={item} classprops="my-2 text-lg" />,
                         )}
                     </ul>
                 )}
